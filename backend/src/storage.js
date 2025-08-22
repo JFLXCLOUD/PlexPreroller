80c9e8dde36a13bce29082cf06d114f704c4bcc9
J_Brns <jeffburns@jflx.cloud>
2025-08-21 22:37:49 -0400

    Update storage.js

--- backend/src/storage.js
+++ backend/src/storage.js
@@ -1,1 +1,46 @@
-<content of storage.js from previous message>
+import fs from "fs/promises";
+import path from "path";
+import mime from "mime-types";
+
+const PREROLL_DIR = process.env.PREROLL_DIR || "/app/prerolls";
+
+async function ensureDir() {
+  await fs.mkdir(PREROLL_DIR, { recursive: true });
+}
+
+async function listPrerolls() {
+  await ensureDir();
+  const entries = await fs.readdir(PREROLL_DIR, { withFileTypes: true });
+  const files = entries.filter(e => e.isFile());
+  return Promise.all(files.map(async f => {
+    const p = path.join(PREROLL_DIR, f.name);
+    const stat = await fs.stat(p);
+    return {
+      name: f.name,
+      path: p,
+      size: stat.size,
+      mime: mime.lookup(p) || "application/octet-stream",
+      mtime: stat.mtime
+    };
+  }));
+}
+
+async function saveUpload({ filename, buffer }) {
+  await ensureDir();
+  const safe = filename.replace(/[^\w.\- ]+/g, "_");
+  const full = path.join(PREROLL_DIR, safe);
+  await fs.writeFile(full, buffer);
+  return full;
+}
+
+async function removePreroll(filename) {
+  const full = path.join(PREROLL_DIR, filename);
+  await fs.unlink(full);
+}
+
+export default {
+  PREROLL_DIR,
+  listPrerolls,
+  saveUpload,
+  removePreroll
+};
